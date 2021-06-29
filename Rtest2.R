3eee8a91cc5e15df49fbc6287badf0ef134b2f9b
tinbqt <quangtin91@gmail.com>
2021-06-29 21:06:57 +0700

    adding the title for R file and defination of iris

--- Rtest2.R
+++ Rtest2.R
@@ -1,4 +1,20 @@
-head (iris)
+############################################
+#File: Rtest2                     
+#Course: Rprograming Tutorial FreeCodeCamp
+#Chatper:
+#Section: plot function
+#Author: tinbqt 
+#Date: 2016-05-24
+############################################
+
+?iris
+
+# his famous (Fisher's or Anderson's) iris data set
+# gives the measurements in centimeters of the variables 
+# sepal length and width and petal length and width, 
+# respectively, for 50 flowers from each of 3 species of iris. 
+# The species are Iris setosa, versicolor, and virginica.
+
 #plot function#
 plot(iris$Species)
 plot(iris$Petal.Length) #plot dot graph
